915f2f9c937ee5de33ced9e79fe53f7bc43cea2c
Richard B. Wagner <breckwagner@gmail.com>
2015-10-25 09:46:33 -0700

    Updated Log and added more test scripts
    - Updated Log
    - Added tor test script to start fixing rate limiting issue with data
    extractor
    - Added stub for README.md which will contain install instructions when
    finished

--- Other/data_extractor_2.py
+++ Other/data_extractor_2.py
@@ -17,29 +17,45 @@ import subprocess
 
 user_list = []
 csv_column = 0
+input_file = 'Medium Reputation QueryResults.csv'
+output_list = []
+seed = '2380830'
 
-with open('Medium Reputation QueryResults.csv', newline='') as csvfile:
+
+
+with open(input_file, newline='') as csvfile:
 	spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
 	for row in spamreader:
 		user_list.append([row[csv_column]])
 
+user_list = user_list[user_list.index([seed]):]
 
-for user_id in range(1,len(user_list)) :
-	page = requests.get('http://stackoverflow.com/users/' + user_list[user_id][0] + '/')
-	tree = html.fromstring(page.text)
+try:
+	for user_id in range(1,len(user_list)) :
+		page = requests.get('http://stackoverflow.com/users/' + user_list[user_id][0] + '/')
+		tree = html.fromstring(page.text)
 
-	subtree = tree.cssselect('.user-links')
+		subtree = tree.cssselect('.user-links')
 
-	#link_list = ['.icon-github + a', '.icon-twitter + a', '.icon-site + a']
-	link_list = ['.icon-github + a']
-	for i in range(len(link_list)) :
-		result = tree.cssselect(link_list[i])
-		if(len(result)==1):
-			print(result[0].text)
-			bashCommand = "./findGitHubEmail " + result[0].text
-			process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-			output = process.communicate()[0]
-			
-			# add email address to list
-			user_list[user_id].append(output)
-			print(user_list[user_id])
+		#link_list = ['.icon-github + a', '.icon-twitter + a', '.icon-site + a']
+		link_list = ['.icon-github + a']
+		for i in range(len(link_list)) :
+			result = tree.cssselect(link_list[i])
+			if(len(result)==1):
+				bashCommand = "./findGitHubEmail " + result[0].text
+				process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+				output = process.communicate()[0]
+				
+				# add email address to list
+				user_list[user_id].append(output)
+				output_list.append(user_list[user_id])
+				print(user_list[user_id])
+			else:
+				user_list[user_id].append("")
+				print(user_list[user_id])
+except:
+	pass
+finally:
+	with open('output.csv', 'w', newline='') as fp:
+		a = csv.writer(fp, delimiter=',')
+		a.writerows(output_list)
